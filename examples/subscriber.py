c589b0301194aa2101ee2ffe0bee7744004ac881
Xiwei Zhao <xiwei.zhao@aalto.fi>
2024-09-20 18:20:06 +0300

    feat: got one step inference result and send back to isaacsim in a for loop sequence

--- examples/subscriber.py
+++ examples/subscriber.py
@@ -1,12 +1,13 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-from std_msgs.msg import Float32MultiArray
-from concurrent.futures import Future
-from rclpy.executors import MultiThreadedExecutor
-from sensor_msgs.msg import Image
-import matplotlib.pyplot as plt
+# from std_msgs.msg import String
+# from std_msgs.msg import Float32MultiArray
+# from concurrent.futures import Future
+# from rclpy.executors import MultiThreadedExecutor
+# from sensor_msgs.msg import Image
+# import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 class Subscriber(Node):
     def __init__(self):
@@ -15,25 +16,26 @@ class Subscriber(Node):
         self.data = None
         self.future = None
 
-    def subscribe_once(self, topic_name='/Camera_rgb'):
-        if self.subscription is None:
-            if topic_name == '/language_topic':
-                self.subscription = self.create_subscription(
-                    String,
-                    topic_name,
-                    self.callback,
-                    10
-                )
-            else:
-                self.subscription = self.create_subscription(
-                    Image,
-                    topic_name,
-                    self.callback,
-                    10
-                )
-            self.future = Future()
+    # def subscribe_once(self, topic_name='/Camera_rgb'):
+    #     if self.subscription is None:
+    #         if topic_name == '/language_topic':
+    #             self.subscription = self.create_subscription(
+    #                 String,
+    #                 topic_name,
+    #                 self.callback,
+    #                 10
+    #             )
+    #         else:
+    #             print("subscribing to image topic")
+    #             self.subscription = self.create_subscription(
+    #                 Image,
+    #                 topic_name,
+    #                 self.callback,
+    #                 10
+    #             )
+    #         self.future = Future()
 
-        return self.future
+    #     return self.future
 
     def callback(self, msg):
         self.data = msg
@@ -44,21 +46,51 @@ class Subscriber(Node):
         self.destroy_subscription(self.subscription)
         self.subscription = None
 
-def sub_call(node, topic='/Camera_rgb',size=(512,512,3)):
-    future = node.subscribe_once(topic)
+def wait_for_message(node ,topic_type, topic): 
+    class _vfm(object):
+        def __init__(self) -> None:
+            self.msg = None
+            
+        def cb(self, msg):
+            self.msg = msg
 
-    executor = MultiThreadedExecutor()
+    vfm = _vfm()
+    subscription = node.create_subscription(topic_type,topic,vfm.cb,1)
     while rclpy.ok():
-        rclpy.spin_once(node, timeout_sec=0.1)
+        if vfm.msg != None: return vfm.msg
+        rclpy.spin_once(node)
+        time.sleep(0.001)
+    # unsubcription
+    subscription.destroy()
+
+def get_observation(node ,topic_type, topic, size=(256,256,3)):
+
+    input_images = wait_for_message(node, topic_type, topic)
+    input_images = np.array(input_images.data).reshape(size)
+
+    input_images = np.stack(input_images)[None]
+    input_images = np.stack(input_images)[None]
+
+    return input_images
+    
+
+
+# def sub_call(node, topic='/Camera_rgb',size=(512,512,3)):
+#     future = node.subscribe_once(topic)
+#     print("get the future")
+
+#     executor = MultiThreadedExecutor()
+#     while rclpy.ok():
+#         rclpy.spin_once(node, timeout_sec=0.1)
         
-        if future.done():
-            break
+#         if future.done():
+#             break
 
-        if not size:
-            return future.result()
+#         if not size:
+#             return future.result()
 
-        msg_result = future.result()
-        return np.array(msg_result.data).reshape(size)
+#         msg_result = future.result()
+#         return np.array(msg_result.data).reshape(size)
 
 # class LanguageSubscriber(Node):
 
